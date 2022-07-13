fd4e648697e05899acfcc3d948439cc4d5662868
Jution Candra Kirana <jutionck@gmail.com>
2022-07-13 10:37:08 +0700

    Go Unit Test with Assert

--- calc_test.go
+++ calc_test.go
@@ -1,37 +1,23 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
 func TestCalculator_Add(t *testing.T) {
-	t.Run("Calculator Add operator testing", func(t *testing.T) {
-		numSample1 := 10
-		numSample2 := 20
+	myCalc := Calculator{Num1: 4, Num2: 4}
+	actual := myCalc.Add()
+	expected := 9
+	assert.Equal(t, expected, actual, "it should return 8")
 
-		calc := Calculator{
-			Num1: numSample1,
-			Num2: numSample2,
-		}
-		if calc.Add() != 30 {
-			t.Error("It should return 30")
-		}
-	})
 }
 func TestCalculator_Sub(t *testing.T) {
-	t.Run("Calculator Subtraction operator testing", func(t *testing.T) {
-		numSample1 := 3
-		numSample2 := 2
-
-		calc := Calculator{
-			Num1: numSample1,
-			Num2: numSample2,
-		}
-		if calc.Sub() != 1 {
-			t.Error("It should return 1")
-		}
-	})
+	myCalc := Calculator{Num1: 5, Num2: 4}
+	actual := myCalc.Sub()
+	expected := 1
+	assert.Equal(t, expected, actual, "it should return 1")
 }
 
 func TestMainApp(t *testing.T) {
